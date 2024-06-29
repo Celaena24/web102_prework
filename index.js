fdc5c546f0972c2bb22b99692ce592c2c88b00eb
Stella Dey <stelladey24@gmail.com>
2024-06-29 19:23:30 +0530

    initial display only shows funded games

--- index.js
+++ index.js
@@ -58,7 +58,8 @@ function addGamesToPage(games) {
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-addGamesToPage(GAMES_JSON);
+addGamesToPage(GAMES_JSON.filter(game => game.pledged >= game.goal));
+
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
